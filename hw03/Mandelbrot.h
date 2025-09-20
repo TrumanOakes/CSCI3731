3faa756b902314fb3cd4e72397ed0857a23b0351
TrumanOakes <143456996+TrumanOakes@users.noreply.github.com>
2025-09-20 12:17:01 -1000

    Add files via upload

---
+++ hw03/Mandelbrot.h
@@ -0,0 +1,7 @@
+#ifndef MANDELBROT_H
+#define MANDELBROT_H
+
+void mandelbrot(double* x, double* y, double a, double b);
+
+int mandelbrot(double a, double b, int max_iter);
+#endif
---
+++ hw03/main.cc
@@ -0,0 +1,32 @@
+#include <iostream>
+#include <cstdio>
+#include "Mandelbrot.h"
+int main(){
+    const int width = 80;
+    const int height = 80;
+    const int max_iter = 1000;
+    double a_min = -1.0;
+    double a_max = 1.0;
+    double b_min = -1.0;
+    double b_max = 1.0;
+    FILE* fp = fopen("mandelBrotASCII.txt", "w");
+    for(int i = 0; i < height; i++){
+        double b = b_min + (b_max - b_min ) * i / (height-1);
+        
+        for(int d = 0; d < width; d++){
+            double a = a_min + (a_max - a_min) * d / (width-1);
+            
+            
+            int iter = mandelbrot(a, b, max_iter);
+            if (iter == max_iter)
+                fprintf(fp, "*");
+            else
+                fprintf(fp, " ");
+                }
+                fprintf(fp, "\n");
+            }
+            fclose(fp);
+            return 0;
+        }
+    
+    
